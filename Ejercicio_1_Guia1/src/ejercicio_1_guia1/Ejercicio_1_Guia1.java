da9b410687e0608fa532d00e56c535b3b58f1eba
DxDzere <74333816+DxDzere@users.noreply.github.com>
2023-03-23 22:40:54 -0300

    inicio del programa

---
+++ Ejercicio_1_Guia1/src/ejercicio_1_guia1/Ejercicio_1_Guia1.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio_1_guia1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Zere
+ */
+public class Ejercicio_1_Guia1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int num1, num2;
+        Scanner leer = new Scanner(System.in);
+        
+        System.out.println("Ingrese 2 numeros");
+        System.out.print("Numero 1: ");
+    }
+    
+}
